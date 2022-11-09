19448bf128905c44a82866c3e1ab2f0dde97fe5e
Rose Pineau <rosepineau@protonmail.com>
2022-11-08 20:20:50 -0500

    Successfully implement JS form validation on email.

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-//Script validates passwords:
-const password = document.querySelector("#password");
+const email = document.querySelector("#email");
 const confirmPassword = document.querySelector("#confirm-password");
-const submitButton = document.querySelector("button");
-const errorText = document.querySelector(".error-text");
 
-submitButton.addEventListener("click", () => {
-  checkPassword(password, confirmPassword);
+email.addEventListener("input", (e) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Your email input is invalid");
+    email.reportValidity("");
+  } else {
+    email.setCustomValidity("");
+  }
 });
 
-function checkPassword(pass1, pass2) {
-  if (pass1.value === pass2.value) {
-    pass2.classList.remove("invalid");
-    pass2.classList.add("valid");
-    submitButton.type = "submit";
-  } else {
-    pass2.classList.remove("valid");
-    pass2.classList.add("invalid");
-    submitButton.type = "button";
-    errorText.removeAttribute("hidden");
-  }
-}
+// //Script validates passwords:
+// const password = document.querySelector("#password");
+// const confirmPassword = document.querySelector("#confirm-password");
+// const submitButton = document.querySelector("button");
+// const errorText = document.querySelector(".error-text");
+
+// submitButton.addEventListener("click", () => {
+//   checkPassword(password, confirmPassword);
+// });
+
+// function checkPassword(pass1, pass2) {
+//   if (pass1.value === pass2.value) {
+//     pass2.classList.remove("invalid");
+//     pass2.classList.add("valid");
+//     submitButton.type = "submit";
+//   } else {
+//     pass2.classList.remove("valid");
+//     pass2.classList.add("invalid");
+//     submitButton.type = "button";
+//     errorText.removeAttribute("hidden");
+//   }
+// }
